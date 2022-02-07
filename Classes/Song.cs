fd8048c2d9655cc3fab90a968916d82d3f638bf4
cf-whyte <cfwhytedev@gmail.com>
2022-02-07 16:50:17 +1300

    Styled song cards, added links, artist, and notes section.

--- Classes/Song.cs
+++ Classes/Song.cs
@@ -5,6 +5,8 @@ namespace Classes
         public int Id { get; set; }
         public string Title { get; set; }
         public string Artist { get; set; }
+        public string Notes { get; set; }
+        public string Link { get; set; }
         public List<Chord> Chords { get; set; }
         public DateTime StartDate { get; set; } = DateTime.Now;
     }
